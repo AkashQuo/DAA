416d880c2d71de17313df2cc269336e7da34101d
SkyCoder <67903158+AkashQuo@users.noreply.github.com>
2023-12-17 20:52:56 +0000

    re search

---
+++ ReSearch.py
@@ -0,0 +1,29 @@
+import re
+
+def extract_alter_operations(sql_string):
+    # Regular expression pattern to match ALTER TABLE statements with various operations
+        alter_pattern = re.compile(r'\bALTER\s+TABLE\s+(\w+)\s+(ADD\s+(\w+\s+\w+\([^)]+\)(?:,\s*)?)|MODIFY\s+(\w+\s+\w+\([^)]+\))|RENAME\s+\w+\s+TO\s+(\w+)|DROP\s+COLUMN\s+(\w+))', re.IGNORECASE)
+
+            # Find all matches in the input string
+                matches = alter_pattern.findall(sql_string)
+
+                    # Extract table and operation details from matches
+                        operations = []
+                            for table, add, _, modify, rename, drop in matches:
+                                    if add:
+                                                columns = re.findall(r'(\w+)\s+\w+\([^)]+\)', add)
+                                                            operations.extend([(table, 'ADD', column) for column in columns])
+                                                                    elif modify:
+                                                                                columns = re.findall(r'(\w+)\s+\w+\([^)]+\)', modify)
+                                                                                            operations.extend([(table, 'MODIFY', column) for column in columns])
+                                                                                                    elif rename:
+                                                                                                                operations.append((table, 'RENAME', rename))
+                                                                                                                        elif drop:
+                                                                                                                                    operations.append((table, 'DROP', drop))
+
+                                                                                                                                        return operations
+
+                                                                                                                                        # Example usage
+                                                                                                                                        sql_query = "ALTER TABLE customers ADD COLUMN id INT, MODIFY COLUMN name VARCHAR(255), RENAME TO clients; ALTER TABLE orders DROP COLUMN product_name;"
+                                                                                                                                        result = extract_alter_operations(sql_query)
+                                                                                                                                        print(result)
